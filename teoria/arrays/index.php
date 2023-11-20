c518c81b258eca43a00597350bf702d3a779665f
GustaSoier <gsampaiosoier0@gmail.com>
2023-11-19 23:28:03 -0300

    PHP
    Usando array

---
+++ teoria/arrays/index.php
@@ -0,0 +1,17 @@
+<?php
+$ingredientes = [
+    'açucar',
+    'farinha de trigo',
+    'ovo',
+    'leite',
+    'fermento em pó'
+];
+
+echo $ingredientes[4]."<br/>";
+
+$numerosDaMegaSena = [10, 14, 27, 34, 8];
+
+echo "Número 1: ".$numerosDaMegaSena[0]."<br/>";
+echo "Número 2: ".$numerosDaMegaSena[1];
+
+
