95322c1e780fd57707979bcdce542345345cbcc8
GustaSoier <gsampaiosoier0@gmail.com>
2023-11-19 23:10:56 -0300

    PHP
    Mesclando Variáveis

---
+++ teoria/mesclandoVariaveis/index.php
@@ -0,0 +1,17 @@
+<!-- primeira maneira -->
+<?php
+$nome = 'Gustavo';
+$sobrenome = 'Soier';
+
+$nomeCompleto = $nome.$sobrenome;
+
+echo $nomeCompleto;
+
+// segunda maneira
+$nome = 'Gustavo';
+$sobrenome = 'Soier';
+$idade = 17;
+
+$frase = "$nome $sobrenome tem $idade anos";
+
+echo $frase;
