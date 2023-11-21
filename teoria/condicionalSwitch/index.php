78d4e4bbbf51e676431a5df8f61efad82426b0d2
GustaSoier <gsampaiosoier0@gmail.com>
2023-11-21 20:52:27 -0300

    PHP
    Aprendendo a usar o condicional Switch

---
+++ teoria/condicionalSwitch/index.php
@@ -0,0 +1,15 @@
+<?php
+$tipo = 'texto';
+
+switch($tipo) {
+    case 'foto':
+        echo 'Exibindo FOTO';
+        break;
+
+    case 'video':
+        echo 'Exibindo VIDEO';
+        break;
+        case 'texto':
+            echo 'Exibindo TEXTO';
+            break;
+}
