eab52513581a006e5ee87c0a822b9ecee4ba4095
GustaSoier <gsampaiosoier0@gmail.com>
2023-11-21 19:39:51 -0300

    PHP
    Aprendendo sobre a condicional IF

---
+++ teoria/condicionalIF/index.php
@@ -0,0 +1,17 @@
+<?php
+$idade = 14;
+
+if($idade < 18) {
+    echo 'Menor de idade';
+}
+else{
+    echo 'Maior de idade';
+}
+
+
+// X < Y - menor que
+// X > Y - maior que
+// X == Y - igual a
+// X != Y - diferente de
+// X >= Y - maior ou igual
+// X <= Y - menor ou igual
