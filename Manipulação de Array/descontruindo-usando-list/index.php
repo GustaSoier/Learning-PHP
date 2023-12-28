d5ff80f5171b2d1088f071e11258067faaea8f23
Gustavo <gsampaiosoier0@gmail.com>
2023-12-27 23:45:41 -0300

    PHP
    Aprendendo a descontruir um array usando list

---
+++ Manipulação de Array/descontruindo-usando-list/index.php
@@ -0,0 +1,6 @@
+<?php
+$array = ['Gustavo', 17, 'suco', 'vermelho'];
+
+list($nome, $idade, $bebida,$cor) = $array;
+
+echo $nome." tem ".$idade." anos e gosta de tomar ".$bebida." com a cor ".$cor;
