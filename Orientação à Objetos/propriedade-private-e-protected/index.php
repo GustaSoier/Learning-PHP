23ba940658ecd0420d7cc4fe25186a77be448d4a
Gustavo <gsampaiosoier0@gmail.com>
2024-01-04 22:59:23 -0300

    PHP
    Aprendendo a propriedade private e protected

---
+++ Orientação à Objetos/propriedade-private-e-protected/index.php
@@ -0,0 +1,44 @@
+<?php
+
+class Post {
+    private int $id;
+    private int $likes;
+
+    protected function setId($i) {
+        $this -> id = $i;
+    }
+
+    public function getId() {
+        return  $this -> id ;
+    }
+
+    public function setLikes($n) {
+        $this -> id = $n;
+    }
+
+    public function getLikes() {
+        return  $this -> likes;
+    }
+}
+
+class Foto extends Post {
+    private $url;
+    
+    public function __construct($id) {
+        $this -> setId($id);
+    }
+
+    public function setUrl($u) {
+        $this -> url =$u;
+    }
+
+    public function getUrl() {
+        return $this -> url;
+    }
+}
+
+$foto = new Foto(20);
+$foto -> setLikes(12);
+$foto -> setUrl('abc');
+
+echo "FOTO: #".$foto -> getId(). " - ".$foto -> getLikes()." likes - ".$foto -> getUrl();
