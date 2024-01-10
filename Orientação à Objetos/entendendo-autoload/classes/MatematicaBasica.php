3356a9752e434a27a2101c48e4481f429bbd689c
Gustavo <gsampaiosoier0@gmail.com>
2024-01-09 22:40:37 -0300

    PHP
    Aprendendo sobre autoload

---
+++ Orientação à Objetos/entendendo-autoload/classes/MatematicaBasica.php
@@ -0,0 +1,9 @@
+<?php
+
+class MatematicaBasica {
+    const VERSION = '1.0';
+
+    public function somar($x, $y) {
+        return $x + $y;
+    }
+}
---
+++ Orientação à Objetos/entendendo-autoload/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'autoload.php';
+
+$m = new MatematicaBasica();
+echo $m -> somar(10, 20);
+
+$x = new Algo();
