da18a3219a86ea737471aade1465cc720f0a941e
Gustavo <gsampaiosoier0@gmail.com>
2024-01-09 22:18:44 -0300

    PHP
    Aprendendo sobre PSR-1

---
+++ Orientação à Objetos/entendendo-a-PSR-1-(1-2)/2)/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'matematica.php';
+
+echo "RESULTADO: ";
+$m = new Matematica();
+echo $m->somar(10, 20);
---
+++ Orientação à Objetos/entendendo-a-PSR-1-(1-2)/2)/matematica.php
@@ -0,0 +1,7 @@
+<?php
+
+class Matematica {
+    public function somar($x, $y) {
+        return $x + $y;
+    }
+}
