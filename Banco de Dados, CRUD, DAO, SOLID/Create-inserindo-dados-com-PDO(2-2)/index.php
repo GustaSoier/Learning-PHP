4ecc42a1c76837a68d84691fd032eeafcfb9917d
Gustavo <gsampaiosoier0@gmail.com>
2024-01-13 18:57:28 -0300

    PHP
    Aprendendo mais sobre o create com PDO

---
+++ Banco de Dados, CRUD, DAO, SOLID/Create-inserindo-dados-com-PDO(2-2)/index.php
@@ -0,0 +1,14 @@
+<?php
+require 'config.php';
+?>
+
+<a href="adicionar.php">ADICIONAR USUÁRIO</a>
+
+<table border="1" width="100%">
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <th>EMAIL</th>
+        <th>AÇÕES</th>
+    </tr>
+</table>
