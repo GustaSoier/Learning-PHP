1e0a78d9081527a0a16911b4f539cd5c1e477b7b
Gustavo <gsampaiosoier0@gmail.com>
2024-01-13 19:56:43 -0300

    PHP
    Aprendendo o UPDATE com PDO

---
+++ Banco de Dados, CRUD, DAO, SOLID/Update-atualizando-dados-com-PDO-(1-2)/editar.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'config.php';
+
+$info = [];
+$id = filter_input(INPUT_GET, 'id');
+if($id) {
+    $sql = $pdo -> prepare("SELECT * FROM usuarios WHERE id = :id");
+    $sql -> bindValue(':id', $id);
+    $sql -> execute();
+    
+    if($sql -> rowCount() >0) {
+        $info = $sql -> fetch( PDO::FETCH_ASSOC );
+    }
+    else {
+        header("Location: index.php");
+    }
+}
+else {
+    header("Location: index.php");
+}
+
+?>
+
+<h1>Editar Usuário</h1>
+<form method="POST" action="editar_action.php">
+    <label>
+        Nome:<br/>
+        <input type="text" name="name" value="<?=$info['nome'];?>">
+    </label><br><br/>
+
+    <label>
+        E-mail:<br/>
+        <input type="email" name="email" value="<?=$info['email'];?>">
+    </label><br><br/>
+
+    <input type="submit" value="Salvar">
+</form>
