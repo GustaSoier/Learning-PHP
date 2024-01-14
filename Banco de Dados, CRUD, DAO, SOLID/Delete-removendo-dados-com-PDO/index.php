53574bd8951c5c6ea92101a083a555dc30daa31f
Gustavo <gsampaiosoier0@gmail.com>
2024-01-14 17:59:09 -0300

    PHP
    Aprendendo a dar DELETE com PDO

---
+++ Banco de Dados, CRUD, DAO, SOLID/Delete-removendo-dados-com-PDO/index.php
@@ -0,0 +1,34 @@
+<?php
+require 'config.php';
+
+$lista = [];
+
+$sql = $pdo -> query("SELECT * FROM usuarios");
+
+if($sql -> rowCount() > 0) {
+    $lista = $sql -> fetchAll(PDO::FETCH_ASSOC);
+}
+
+?>
+
+<a href="adicionar.php">ADICIONAR USUÁRIO</a>
+
+<table border="1" width="100%">
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <th>EMAIL</th>
+        <th>AÇÕES</th>
+    </tr>
+    <?php foreach($lista as $usuario): ?>
+        <tr>
+            <td><?=$usuario['id'];?></td>
+            <td><?=$usuario['nome'];?></td>
+            <td><?=$usuario['email'];?></td>
+            <td>
+                <a href="editar.php?id=<?=$usuario['id'];?>">[ Editar ]</a>
+                <a href="excluir.php?id=<?=$usuario['id'];?>" onclick="return confirm('Tem certeza que deseja excluir?')">[ Excluir ]</a>
+            </td>
+    <?php endforeach; ?>
+    
+</table>
