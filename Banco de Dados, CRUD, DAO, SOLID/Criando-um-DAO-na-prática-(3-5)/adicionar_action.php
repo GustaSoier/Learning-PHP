9515ee64bb51b5c5e5c74e3e5bdb883e7d36c9f3
Gustavo <gsampaiosoier0@gmail.com>
2024-01-17 23:38:49 -0300

    PHP
    Aprendendo a criar um DAO na prática (3/5)

---
+++ Banco de Dados, CRUD, DAO, SOLID/Criando-um-DAO-na-prática-(3-5)/adicionar_action.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'config.php';
+
+$name = filter_input(INPUT_POST, 'name');
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+if($name && $email) {
+
+    $sql = $pdo -> prepare("SELECT * FROM usuarios WHERE email = :email");
+    $sql -> bindValue(':email', $email);
+    $sql -> execute();
+
+    if($sql -> rowCount() === 0) {
+    $sql = $pdo -> prepare("INSERT INTO usuarios (nome, email) VALUES (:name, :email)");
+    $sql -> bindValue(':name', $name);
+    $sql -> bindParam(':email', $email);
+    $sql -> execute();
+    
+    header("Location: index.php");
+    exit;
+    }
+    else {
+        header("Location: adicionar.php");
+        exit;
+    }
+
+}
+
+else {
+    header("Location: adicionar.php");
+    exit;
+}
