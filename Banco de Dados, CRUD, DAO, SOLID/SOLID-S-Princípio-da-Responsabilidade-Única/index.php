8acb340ab9288b1b3314d2cb31a58b58947f678e
Gustavo <gsampaiosoier0@gmail.com>
2024-01-24 22:29:00 -0300

    PHP
    Aprendendo o conceito SOLID (S)

---
+++ Banco de Dados, CRUD, DAO, SOLID/SOLID-S-Princípio-da-Responsabilidade-Única/index.php
@@ -0,0 +1,21 @@
+<?php
+
+class Usuario {
+    public function setNome() {}
+    public function getNome() {}
+
+    public function add() {}
+    public function update() {}
+    public function delete() {}
+}
+
+class Usuario {
+    public function setNome() {}
+    public function getNome() {}
+}
+
+class UsuarioDb {
+    public function add(Usuario $u) {}
+    public function update(Usuario $u) {}
+    public function delete(Usuario $u) {}
+}
